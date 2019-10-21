b5efda50f1f0496a394469a531334c0cda91c6e6
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-21 18:11:46 -0300

    Creation new models

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -25,6 +25,10 @@ class User extends Model {
 		return this;
 	}
 
+	static associate(models) {
+		this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+	}
+
 	chekckPassword(password) {
 		return bcrypt.compare(password, this.password_hash);
 	}
