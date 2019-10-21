d55c1e07f0083f2ced7e4060304539fb3e82300d
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-21 18:14:22 -0300

    Change the configuration

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
 	dialect: 'postgres',
 	host: 'localhost',
 	username: 'postgres',
-	password: 'lol121512',
+	password: 'docker',
 	database: 'gobarber',
 	define: {
 		timestamps: true,
