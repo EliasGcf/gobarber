db4b4d67054a74f950ae3127aa720d5de45f0a5a
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-21 18:14:06 -0300

    Adding new models

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,10 +1,12 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
 	constructor() {
@@ -14,7 +16,9 @@ class Database {
 	init() {
 		this.connection = new Sequelize(databaseConfig);
 
-		models.map(model => model.init(this.connection));
+		models
+			.map(model => model.init(this.connection))
+			.map(model => model.associate && model.associate(this.connection.models));
 	}
 }
 
