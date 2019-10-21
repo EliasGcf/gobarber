075caba73ba70a793a20297be1b13d606d89d72a
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-21 18:13:17 -0300

    Adding Routes to Images

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 import './database';
@@ -13,6 +14,10 @@ class App {
 
 	middlewares() {
 		this.server.use(express.json());
+		this.server.use(
+			'/files',
+			express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+		);
 	}
 
 	routes() {
