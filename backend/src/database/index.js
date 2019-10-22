4f19b29a272866cb54e5f1b25b7c6bd9ce1f9108
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-22 18:28:11 -0300

    Setting for MongoDB

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
 	constructor() {
 		this.init();
+		this.mongo();
 	}
 
 	init() {
@@ -19,6 +21,17 @@ class Database {
 		models
 			.map(model => model.init(this.connection))
 			.map(model => model.associate && model.associate(this.connection.models));
+	}
+
+	mongo() {
+		this.mongoConnection = mongoose.connect(
+			'mongodb://localhost:27017/gobarber',
+			{
+				useNewUrlParser: true,
+				useFindAndModify: true,
+				useUnifiedTopology: true,
+			}
+		);
 	}
 }
 
