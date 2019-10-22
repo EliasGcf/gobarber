59ff2d729e1101258beb89530ff8fd4b3d349ec3
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-22 18:27:38 -0300

    Setting Cancellation Mail

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	secure: false,
+	auth: {
+		user: '4117751bf2332d',
+		pass: 'd7a5ecc44be818',
+	},
+	default: {
+		from: 'Equipe GoBarber  <noreply@gobarber.com>',
+	},
+};
