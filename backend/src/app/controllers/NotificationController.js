aea49df4595cfa9ace1d2ebb9b07a22fde5658a0
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-22 18:22:57 -0300

    Creation new controllers

---
+++ backend/src/app/controllers/NotificationController.js
@@ -0,0 +1,36 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificationController {
+	async index(req, res) {
+		const checkIsProvider = await User.findOne({
+			where: { id: req.userId, provider: true },
+		});
+
+		if (!checkIsProvider) {
+			return res
+				.status(401)
+				.json({ error: 'Only provider can load notifications' });
+		}
+
+		const notifications = await Notification.find({
+			user: req.userId,
+		})
+			.sort({ createdAt: 'desc' })
+			.limit(20);
+
+		return res.json(notifications);
+	}
+
+	async update(req, res) {
+		const notification = await Notification.findByIdAndUpdate(
+			req.params.id,
+			{ read: true },
+			{ new: true }
+		);
+
+		return res.json(notification);
+	}
+}
+
+export default new NotificationController();
