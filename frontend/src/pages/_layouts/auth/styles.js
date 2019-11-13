a3d09a1a2693d21406636cce02d472fe64f00e20
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-13 09:44:58 -0300

    Layouts por página

---
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	height: 100%;
+	background: linear-gradient(-90deg, #7159c1, #ab59c1);
+`;
--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
 	component: Component,
 	isPrivate,
 	...rest
 }) {
-	const signed = false;
+	const signed = true;
 
 	if (!signed && isPrivate) {
 		return <Redirect to="/" />;
@@ -17,7 +20,18 @@ export default function RouteWrapper({
 		return <Redirect to="/dashboard" />;
 	}
 
-	return <Route {...rest} component={Component} />;
+	const Layout = signed ? DefaultLayout : AuthLayout;
+
+	return (
+		<Route
+			{...rest}
+			render={props => (
+				<Layout>
+					<Component {...props} />
+				</Layout>
+			)}
+		/>
+	);
 }
 
 RouteWrapper.propTypes = {
