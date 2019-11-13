2594ad7e149cc916d2ae4e89fff3dad1a41a967f
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-13 10:44:02 -0300

    Utilizando Unform

--- frontend/src/pages/SingIn/index.js
+++ frontend/src/pages/SingIn/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SingIn() {
+	function handleSubmit(data) {
+		console.tron.log(data);
+	}
+
 	return (
 		<>
 			<img src={logo} alt="GoBarber" />
 
-			<form>
-				<input type="email" placeholder="Seu e-mail" />
-				<input type="password" placeholder="Sua senha secreta" />
+			<Form onSubmit={handleSubmit}>
+				<Input name="email" type="email" placeholder="Seu e-mail" />
+				<Input
+					name="password"
+					type="password"
+					placeholder="Sua senha secreta"
+				/>
 
 				<button type="submit">Acessar</button>
 				<Link to="/register">Criar conta gratuita</Link>
-			</form>
+			</Form>
 		</>
 	);
 }
--- frontend/src/pages/SingUp/index.js
+++ frontend/src/pages/SingUp/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SingUp() {
+	function handleSubmit(data) {
+		console.tron.log(data);
+	}
+
 	return (
 		<>
 			<img src={logo} alt="GoBarber" />
 
-			<form>
-				<input placeholder="Nome completo" />
-				<input type="email" placeholder="Seu e-mail" />
-				<input type="password" placeholder="Sua senha secreta" />
+			<Form onSubmit={handleSubmit}>
+				<Input name="name" placeholder="Nome completo" />
+				<Input name="email" type="email" placeholder="Seu e-mail" />
+				<Input
+					name="password"
+					type="password"
+					placeholder="Sua senha secreta"
+				/>
 
 				<button type="submit">Criar conta</button>
 				<Link to="/">Já tenho login</Link>
-			</form>
+			</Form>
 		</>
 	);
 }
