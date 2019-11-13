85e74b7b97871fd16245c7b72919300a3d78cf0f
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-13 11:23:04 -0300

    Configurando store - Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
+
 import Routes from './routes';
 import history from './services/history';
+
+import store from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
 	return (
-		<Router history={history}>
-			<Routes />
-			<GlobalStyle />
-		</Router>
+		<Provider store={store}>
+			<Router history={history}>
+				<Routes />
+				<GlobalStyle />
+			</Router>
+		</Provider>
 	);
 }
 
---
+++ frontend/src/store/index.js
@@ -0,0 +1,20 @@
+import createSagaMiddleware from 'redux-saga';
+import createStore from './createStore';
+
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMonitor =
+	process.env.NODE_ENV === 'development'
+		? console.tron.createSagaMonitor()
+		: null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(rootReducer, middlewares);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
