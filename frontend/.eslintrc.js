f4f1fb4f26857355b5cfef080fbd694b4607d7b7
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-13 10:10:02 -0300

    Configurando Root Import

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -34,11 +34,11 @@ module.exports = {
 		"react-hooks/exhaustive-deps": "warn",
 		'react/jsx-props-no-spreading': 'off',
   },
-  /* settings: {
+  settings: {
     "import/resolver": {
       "babel-plugin-root-import": {
         rootPathSuffix: "src"
       },
     },
-  }, */
+  },
 };
--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouteWrapper({
 	component: Component,
