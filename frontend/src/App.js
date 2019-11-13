3095bfbfe74218955eb7a86e7d3d6a18c30e3c89
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-13 09:52:32 -0300

    Configurando GlobalStyle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,13 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 function App() {
 	return (
 		<Router history={history}>
 			<Routes />
+			<GlobalStyle />
 		</Router>
 	);
 }
--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -10,7 +10,7 @@ export default function RouteWrapper({
 	isPrivate,
 	...rest
 }) {
-	const signed = true;
+	const signed = false;
 
 	if (!signed && isPrivate) {
 		return <Redirect to="/" />;
