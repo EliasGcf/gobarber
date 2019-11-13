e8d5cdb74bc378d8fc7764903ed760f2d1c8434e
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-13 09:01:45 -0300

    Configurando Reacotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
+import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
