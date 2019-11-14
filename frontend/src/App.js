9fb44a80c936a07c3401ea4c26667be93d685e0a
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-14 10:10:32 -0300

    Persistindo autenticação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
@@ -7,17 +8,19 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
 	return (
 		<Provider store={store}>
-			<Router history={history}>
-				<Routes />
-				<GlobalStyle />
-			</Router>
+			<PersistGate persistor={persistor}>
+				<Router history={history}>
+					<Routes />
+					<GlobalStyle />
+				</Router>
+			</PersistGate>
 		</Provider>
 	);
 }
--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -5,7 +5,7 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
-import store from '~/store';
+import { store } from '~/store';
 
 export default function RouteWrapper({
 	component: Component,
