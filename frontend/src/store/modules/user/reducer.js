43384e356fb7b5a9bf53783a84f42f709bb6b858
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-14 09:57:34 -0300

    Armazenando perfil

---
+++ frontend/src/store/modules/user/reducer.js
@@ -0,0 +1,16 @@
+import produce from 'immer';
+
+const INITIAL_STATE = {
+	profile: null,
+};
+
+export default function user(state = INITIAL_STATE, action) {
+	switch (action.type) {
+		case '@auth/SIGN_IN_SUCCESS':
+			return produce(state, draft => {
+				draft.profile = action.payload.user;
+			});
+		default:
+			return state;
+	}
+}
