8f82648d5610be5478aab71f6ba9963515056525
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-15 15:59:04 -0300

    Estilização do dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+// import api from '~/services/api';
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-	return <h1>Dashboard</h1>;
+	return (
+		<Container>
+			<header>
+				<button type="button">
+					<MdChevronLeft size={36} color="#fff" />
+				</button>
+				<strong>31 de Maio</strong>
+				<button type="button">
+					<MdChevronRight size={36} color="#fff" />
+				</button>
+			</header>
+
+			<ul>
+				<Time past>
+					<strong>08:00</strong>
+					<span>Diego Fernandes</span>
+				</Time>
+				<Time available>
+					<strong>09:00</strong>
+					<span>Em aberto</span>
+				</Time>
+				<Time>
+					<strong>10:00</strong>
+					<span>Diego Fernandes</span>
+				</Time>
+				<Time>
+					<strong>11:00</strong>
+					<span>Diego Fernandes</span>
+				</Time>
+			</ul>
+		</Container>
+	);
 }
