77ea071ec355d52c62b072f34e450f852bf64a65
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-15 18:26:35 -0300

    Corrigindo erro em isEqual

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -6,10 +6,10 @@ import {
 	setHours,
 	setMinutes,
 	setSeconds,
+	setMilliseconds,
 	isBefore,
 	isEqual,
 	parseISO,
-	getHours,
 } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
@@ -38,14 +38,17 @@ export default function Dashboard() {
 			const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 			const data = range.map(hour => {
-				const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+				const checkDate = setMilliseconds(
+					setSeconds(setMinutes(setHours(date, hour), 0), 0),
+					0
+				);
 				const compareDate = utcToZonedTime(checkDate, timezone);
 
 				return {
 					time: `${hour}:00h`,
 					past: isBefore(compareDate, new Date()),
 					appointment: response.data.find(a =>
-						isEqual(hour, getHours(parseISO(a.date)))
+						isEqual(parseISO(a.date), compareDate)
 					),
 				};
 			});
