3dd6bf7ef1a13c4b33dbb50540b7d516438e54ba
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-12-25 20:44:45 -0300

    Estilizando pagina de Login

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
-// import { Container } from './styles';
+
+import {
+	Container,
+	Form,
+	FormInput,
+	SubmitButton,
+	SignLink,
+	SignLinkText,
+} from './styles';
 
 export default function SignIn() {
 	return (
 		<Background>
-			<Text>SignIn</Text>
+			<Container>
+				<Image source={logo} />
 
-			<Input
-				style={{ marginTop: 30 }}
-				icon="call"
-				placeholder="Digite seu nome"
-			/>
-			<Button>Entar</Button>
+				<Form>
+					<FormInput
+						icon="mail-outline"
+						keyboardType="email-address"
+						autoCorrect={false}
+						autoCapitalize="none"
+						placeholder="Digite seu e-mail"
+					/>
+					<FormInput
+						icon="lock-outline"
+						secureTextEntry
+						placeholder="Sua senha secreta"
+					/>
+
+					<SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+				</Form>
+
+				<SignLink onPress={() => {}}>
+					<SignLinkText>Criar conta gratuita</SignLinkText>
+				</SignLink>
+			</Container>
 		</Background>
 	);
 }
---
+++ mobile/src/pages/SignIn/styles.js
@@ -0,0 +1,38 @@
+import { Platform } from 'react-native';
+import styled from 'styled-components/native';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+export const Container = styled.KeyboardAvoidingView.attrs({
+	enabled: Platform.OS === 'ios',
+	behavior: 'padding',
+})`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+	padding: 0 30px;
+`;
+
+export const Form = styled.View`
+	align-self: stretch;
+	margin-top: 50px;
+`;
+
+export const FormInput = styled(Input)`
+	margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+	margin-top: 5px;
+`;
+
+export const SignLink = styled.TouchableOpacity`
+	margin-top: 20px;
+`;
+
+export const SignLinkText = styled.Text`
+	color: #fff;
+	font-weight: bold;
+	font-size: 16px;
+`;
