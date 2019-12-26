01af9e4889602fa0ec9c42bcb30b91042882e5af
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-12-25 21:07:41 -0300

    Estilizando StatusBar

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import Routes from './routes';
 
 export default function src() {
-	return <Routes />;
+	return (
+		<>
+			<StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+			<Routes />
+		</>
+	);
 }
