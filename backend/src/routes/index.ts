bc7442685c6e6d8c520dbfa75e7809bcb2ca1205
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-04-17 18:40:51 -0300

    Corrigindo nome

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import appointmentsRouter from './appointments.routes';
-import usersRouter from './user.routes';
+import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
 
 const routes = Router();
