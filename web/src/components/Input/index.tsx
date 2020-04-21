1803a8cebcd9b7746af0c012cab08de612b51eec
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-04-21 13:30:06 -0300

    Criando tooltip de erros

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -6,9 +6,10 @@ import React, {
   InputHTMLAttributes,
 } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -40,7 +41,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container isField={isField} isFocused={isFocused}>
+    <Container isErrored={!!error} isField={isField} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
@@ -50,7 +51,11 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         {...rest}
       />
 
-      {error}
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="c53030" size={20} />
+        </Error>
+      )}
     </Container>
   );
 };
