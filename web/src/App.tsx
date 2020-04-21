de19ec22e56e6484623cda90fd15816da2fb3286
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-04-21 15:09:13 -0300

    Criando hook useAuth

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,7 +5,7 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => {
   return (
