9a1340c1c545d0605fba18af5a4489526778cf7e
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-04-23 09:14:19 -0300

    Fix autofill css

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -26,4 +26,13 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
+
+  /* input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active  {
+    box-shadow: 0 0 0px 1000px #232129 inset;
+    transition: "color 9999s ease-out, background-color 9999s ease-out";
+    transition-delay: 9999s;
+  } */
 `;
